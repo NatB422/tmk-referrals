2d14807264406e08bdda585da495b594d836efe2
Nathan Brown <nathan.brown@paperround.net>
2025-10-04 12:40:48 +0100

    Added logo

--- pages/retailer.tsx
+++ pages/retailer.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-import QRCode from 'react-qr-code'
+import dynamic from 'next/dynamic'
+
+const QRCodeLogo = dynamic(() => import('react-qrcode-logo').then(mod => mod.QRCode), { ssr: false })
 
 export default function Retailer() {
   const [postcode, setPostcode] = useState('')
@@ -32,7 +34,22 @@ export default function Retailer() {
         <div style={{ marginTop: 20 }}>
           <div>Referral code: <strong>{result.code}</strong></div>
           <div style={{ background: 'white', display: 'inline-block', padding: 8, marginTop: 8 }}>
-            <QRCode value={`${window.location.origin}/ref/${result.code}`} />
+            {typeof window !== 'undefined' && (() => {
+              const QR: any = QRCodeLogo as any
+              return (
+                <QR
+                  value={`${window.location.origin}/ref/${result.code}`}
+                  size={256}
+                  // using the external logo the user requested
+                  logoImage={'https://themightykitchen.com/cdn/shop/t/6/assets/favicon.ico'}
+                  logoWidth={56}
+                  logoHeight={56}
+                  logoOpacity={1}
+                  logoPadding={6}
+                  removeQrCodeBehindLogo={false}
+                />
+              )
+            })()}
           </div>
           <div style={{ marginTop: 8 }}>Link: <a href={`/?referral=${result.code}`}>{`${window.location.origin}/?referral=${result.code}`}</a></div>
         </div>
